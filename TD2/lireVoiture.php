286d5dc6007f346151f398d78a9509d85d862953
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-17 21:44:06 +0200

    maj
    maj

--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -1,7 +1,17 @@
 <?php
 require_once "Model.php";
- $rep =  PDO::query('SELECT * FROM voiture');
+$rep = $bdd->query('SELECT * FROM voiture'); // $rep=Model::$pdo->query($sql);
+//$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ);
+//$tab_obj=$tab_obj[0]->AUTO_INCREMENT-1;
+/*
+foreach ($tab_obj[0] as $key => $useless){
+    print "<th>$key</th>";*/
 
-$res = $db->query('SELECT * FROM voiture');
-PDO Model::$pdo
+$resultat = $mysqli->query($rep);
+		while ($ligne = $resultat->fetch_assoc()) {
+			echo $ligne['immatriculation'].' '.$ligne['couleur'].' '.$ligne['marque'].'<br> ';
+			
+		}
+		$mysqli->close(); 
+
 ?>
