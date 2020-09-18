de82a2f6a940821420cf20ca434f36573a8361e8
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-18 21:46:09 +0200

    TD2 : Exercice 7

---
+++ TD2/Voiture.php
@@ -0,0 +1,67 @@
+<?php
+class Voiture {
+   
+  private $marque;
+  private $couleur;
+  private $immatriculation;
+      
+  // un getter      
+  public function getMarque() {
+       return $this->marque;  
+  }
+
+  public function getCouleur() {
+       return $this->couleur;  
+  }
+
+  public function getImmatriculation() {
+       return $this->immatriculation;  
+  } 
+
+     
+  // un setter 
+  public function setMarque($marque2) {
+       $this->marque = $marque2;
+  }
+
+  public function setCouleur($couleur2) {
+       $this->couleur = $couleur2;
+  }
+
+  public function setImmatriculation($immatriculation2) {
+       if(strlen($immatriculation2)==8){
+        $this->immatriculation = $immatriculation2;
+        }
+  }
+      
+  // un constructeur
+  /*
+  public function __construct($m, $c, $i)  {
+   $this->marque = $m;
+   $this->couleur = $c;
+   $this->immatriculation = $i;
+  } */
+
+  public function __construct($m = NULL, $c = NULL, $i = NULL) {
+  if (!is_null($m) && !is_null($c) && !is_null($i)) {
+    // Si aucun de $m, $c et $i sont nuls,
+    // c'est forcement qu'on les a fournis
+    // donc on retombe sur le constructeur à 3 arguments
+    $this->marque = $m;
+    $this->couleur = $c;
+    $this->immatriculation = $i;
+  }
+}
+           
+  // une methode d'affichage.
+  public function afficher() {
+      echo $this->marque;
+      echo "<br>";
+      echo $this->couleur;
+      echo "<br>";
+      echo $this->immatriculation;
+
+  }
+}
+?>
+
--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -1,22 +1,21 @@
 <?php
 require_once "Model.php";
-$rep = Model::$pdo->query("SELECT * FROM voiture"); // 
- //va chercher les infos de la requete
-//$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ)
+require_once "Voiture.php";
+
+$rep = Model::$pdo->query("SELECT * FROM voiture"); 
+/*
+//$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ) //va chercher les infos de la requete
 while($tab_obj = $rep->fetch()){
 	echo $tab_obj['immatriculation']." ".$tab_obj['couleur']." ".$tab_obj['marque'].'<br> ';;
 }
+*/
 
-//$tab_obj=$tab_obj[0]->AUTO_INCREMENT-1;
-/*
-foreach ($tab_obj[0] as $key => $useless){
-    print "<th>$key</th>";
+$rep->setFetchMode(PDO::FETCH_CLASS, 'voiture');
+//$tab_voit = $rep->fetchAll();
+while($tab_voit = $rep->fetchAll()){
+	$tab_voit->afficher();
+}
 
-$resultat = $mysqli->query($rep);
-		while ($ligne = $resultat->fetch_assoc()) {
-			echo $ligne['immatriculation'].' '.$ligne['couleur'].' '.$ligne['marque'].'<br> ';
-			
-		}
-		$mysqli->close(); */
+
 
 ?>
