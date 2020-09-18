0d12618b8d31417bedb08d1905099a98c0aac9a0
samuel-robinet <samuel.robinet@etu.umontpellier.fr>
2020-09-18 23:49:31 +0200

    TD3 : Exercice 1

---
+++ TD3/Voiture.php
@@ -0,0 +1,105 @@
+<?php
+class Voiture {
+   
+  private $marque;
+  private $couleur;
+  private $immatriculation;
+      
+  // un getter      
+  public function getMarque() {
+       return $this->marque;  
+  }
+
+  public function getCouleur() {
+       return $this->couleur;  
+  }
+
+  public function getImmatriculation() {
+       return $this->immatriculation;  
+  } 
+
+     
+  // un setter 
+  public function setMarque($marque2) {
+       $this->marque = $marque2;
+  }
+
+  public function setCouleur($couleur2) {
+       $this->couleur = $couleur2;
+  }
+
+  public function setImmatriculation($immatriculation2) {
+       if(strlen($immatriculation2)==8){
+        $this->immatriculation = $immatriculation2;
+        }
+  }
+      
+  // un constructeur
+  /*
+  public function __construct($m, $c, $i)  {
+   $this->marque = $m;
+   $this->couleur = $c;
+   $this->immatriculation = $i;
+  } */
+
+  public function __construct($m = NULL, $c = NULL, $i = NULL) {
+  if (!is_null($m) && !is_null($c) && !is_null($i)) {
+    // Si aucun de $m, $c et $i sont nuls,
+    // c'est forcement qu'on les a fournis
+    // donc on retombe sur le constructeur à 3 arguments
+    $this->marque = $m;
+    $this->couleur = $c;
+    $this->immatriculation = $i;
+    }
+  }
+
+  public static function getAllVoitures() { 
+      try {
+       $sql = "SELECT * from voiture";
+      } catch (PDOException $e) {
+        if (Conf::getDebug()) {
+          echo $e->getMessage(); // affiche un message d'erreur
+        } else {
+          echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';}
+        die();
+        }
+
+      $rep = Model::$pdo->query($sql);
+      $rep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+      return $rep->fetchAll(); 
+  }
+
+  public static function getVoitureByImmat($immat) {
+    $sql = "SELECT * from voiture WHERE immatriculation=:nom_tag";
+    // Préparation de la requête
+    $req_prep = Model::$pdo->prepare($sql);
+
+    $values = array(
+        "nom_tag" => $immat,
+        //nomdutag => valeur, ...
+        );
+    
+    // On donne les valeurs et on exécute la requête   
+    $req_prep->execute($values);
+
+    // On récupère les résultats comme précédemment
+    $req_prep->setFetchMode(PDO::FETCH_CLASS, 'Voiture');
+    $tab_voit = $req_prep->fetchAll();
+    // Attention, si il n'y a pas de résultats, on renvoie false
+    if (empty($tab_voit))
+        return false;
+    return $tab_voit[0];
+}
+
+  // une methode d'affichage.
+  public function afficher() {
+      echo $this->marque;
+      echo "<br>";
+      echo $this->couleur;
+      echo "<br>";
+      echo $this->immatriculation;
+
+  }
+}
+?>
+
---
+++ TD3/lireVoiture.php
@@ -0,0 +1,54 @@
+<?php
+require_once "Model.php";
+require_once "Voiture.php";
+
+$rep = Model::$pdo->query("SELECT * FROM voiture"); 
+/*
+//$tab_obj = $rep->fetchAll(PDO::FETCH_OBJ) //va chercher les infos de la requete
+while($tab_obj = $rep->fetch()){
+	echo $tab_obj['immatriculation']." ".$tab_obj['couleur']." ".$tab_obj['marque'].'<br> ';;
+}
+*/
+
+$rep->setFetchMode(PDO::FETCH_CLASS, 'voiture');
+//$tab_voit = $rep->fetchAll();
+while($tab_voit = $rep->fetch()){
+	$tab_voit->afficher();
+	echo '<br>';
+	echo '<br>';
+
+}
+
+echo '<br>';
+echo '<br>';
+
+$donnees = voiture::getAllVoitures();
+foreach($donnees as $uneLigne){
+    $uneLigne->afficher();
+    echo '<br>';
+	echo '<br>';
+
+}
+
+$donnees = voiture::getVoitureByImmat('08787975');
+$donnees->afficher();
+/*
+while ($donnees = $reponse->fetch()
+{
+   // Ton traitement
+}
+équivaut à 
+
+1
+2
+3
+4
+5
+$donnees = $reponse->fetchAll();
+foreach($donnees as $uneLigne)
+{
+    // Ton traitement
+}
+*/
+
+?>
