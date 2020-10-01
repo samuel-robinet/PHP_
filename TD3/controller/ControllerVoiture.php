9bed68b6742e47ca1abbb26f201706beec61dd97
Samuel Robinet <Samuel.robinet@etu.umontpellier.fr>
2020-10-01 09:50:05 +0200

    4 TD4

--- TD3/controller/ControllerVoiture.php
+++ TD3/controller/ControllerVoiture.php
@@ -1,5 +1,15 @@
 <?php
+
+ /*
 require_once ('../model/ModelVoiture.php'); // chargement du modèle
 $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
-require ('../view/voiture/list.php');  //redirige vers la vue
+require ('../view/voiture/list.php');  //redirige vers la vue */
+
+require_once ('../model/ModelVoiture.php'); // chargement du modèle
+class ControllerVoiture {
+    public static function readAll() {
+        $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
+        require ('../view/voiture/list.php');  //"redirige" vers la vue
+    }
+}
 ?>
